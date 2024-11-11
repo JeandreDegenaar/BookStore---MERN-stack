c56ed65b57347d9f3bbafa45e041328398711b6a
Jeandre Degenaar <jeandredegenaar@gmail.com>
2024-11-11 06:17:17 +0200

    first commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://jeandredegenaar:93XP1fJkRAkU9dUh@book-store-mern.ey2as.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
